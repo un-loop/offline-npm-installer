7e06f4f938961e969e970e93f5ff8900f8422a67
Joshua Goldberg <josh@fullscreenmario.com>
2018-07-25 10:32:33 -0700

    Added prettier

--- src/main.ts
+++ src/main.ts
@@ -6,22 +6,22 @@ import { app, BrowserWindow } from "electron";
 let mainWindow: BrowserWindow | undefined;
 
 const createWindow = () => {
-    // Create the browser window.
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-    // and load the index.html of the app.
-    mainWindow.loadFile("index.html");
+  // and load the index.html of the app.
+  mainWindow.loadFile("index.html");
 
-    // Open the DevTools.
-    // mainWindow.webContents.openDevTools()
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 
-    // Emitted when the window is closed.
-    mainWindow.on("closed", () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        mainWindow = undefined;
-    });
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = undefined;
+  });
 };
 
 // This method will be called when Electron has finished
@@ -31,19 +31,19 @@ app.on("ready", createWindow);
 
 // Quit when all windows are closed.
 app.on("window-all-closed", () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
 app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainWindow === undefined) {
-        createWindow();
-    }
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === undefined) {
+    createWindow();
+  }
 });
 
 // In this file you can include the rest of your app's specific main process
